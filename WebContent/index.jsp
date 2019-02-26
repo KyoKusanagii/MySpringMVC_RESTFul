512b3b1eb8c1cf3793f09b0d5cc525b69299c561
KyoKusanagii <44694954+KyoKusanagii@users.noreply.github.com>
2019-02-26 22:47:59 +0800

    修改SpringRestful專案

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -6,8 +6,54 @@
 <head>
 <meta charset="BIG5">
 <title>Insert title here</title>
+<script
+		src="https://code.jquery.com/jquery-1.12.4.min.js"
+		integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+		crossorigin="anonymous">
+</script>
+<script>
+	$(function () {
+		$("testJson").click(function () {
+			let url = $(this).href;
+			let args = {};
+			$.post(url,args,function (data) {
+				for(let i=0;i<data.length;i++){
+					let id = data[i].empId;
+					let age = data[i].age;
+					let name = data[i].name;
+					console.log("id=" + id + ",age = " + age + ",name = " + name + "\n");
+				}
+			});
+			return false;
+		});
+	})
+	
+</script>
 </head>
 <body>
 	<a href="<%=request.getContextPath()%>/emps">秀出全部員工</a>
+	<br><br>
+	<a href="testJson" id="testJson">測試JSON</a>
+	<br><br>
+	<form action="testHttpMessageConverter" method="post" enctype="multipart/form-data">
+		檔案:<input type="file" name="file">
+		描述:<input type="text" name="desc">
+		<input type="submit">
+	</form>
+	<br><br>
+	<a href="testResponseEntity">測試ResponseEntity</a>
+
+	<!--
+		關於國際化:
+		1.在頁面上能夠根據瀏覽器語言設置的情況對文本(不是內容)，時間、數值進行本地化處理
+		2.可以在bean中獲取國際化資源文件locale對應的消息
+		3.可以透過超連結切換locale，而不再依賴於瀏覽器的語言設置情況
+
+		解決:
+		1.使用JSTL的fmt標籤解決
+		2.在bean中注入ResourceBundleMessageSource的實例，使用其對應的getMessage方法即可
+		3.配置LocaleResolver和LocaleChangeInterceptor
+	-->
+	<a href="i18n">i18n 頁面</a>
 </body>
 </html>
--- src/com/max/springmvc/converter/SpringMVCTest.java
+++ src/com/max/springmvc/converter/SpringMVCTest.java
@@ -3,15 +3,59 @@ package com.max.springmvc.converter;
 import com.max.springmvc.restful.model.EmployeeDAO;
 import com.max.springmvc.restful.model.EmployeeVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class SpringMVCTest {
 
     @Autowired
     private EmployeeDAO emdao;
+
+    @RequestMapping(value = "/testResponseEntity")
+    public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException {
+        byte [] body = null;
+        ServletContext servletContext = session.getServletContext();
+        InputStream is = servletContext.getResourceAsStream("/files/article.txt");
+        body = new byte[is.available()];
+        is.read(body);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cotent-Disposition","attachment;filename=article.txt");  //下載
+
+        HttpStatus statusCode = HttpStatus.OK;
+
+
+        ResponseEntity<byte[]> response = new ResponseEntity<>(body,headers,statusCode);
+        return response;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/testHttpMessageConverter")
+    public String testHttpMessageConverter(@RequestBody String body){
+        System.out.println("body = " + body);
+        return "helloworld" + new Date();   //return的字串不是返回的頁面名稱，而是把字直接回傳給客戶端
+    }
+
+    @ResponseBody   ///可以把請求結果寫入Http的ResponseBody
+    @RequestMapping("/testJson")
+    public List<EmployeeVO> testJson(){
+
+        return emdao.getAll();
+    }
 
     @RequestMapping(value = "/testConversionServiceConverter")
     public String testConverter(@RequestParam("employee") EmployeeVO employee){
